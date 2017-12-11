d61bdc8577b87abddeec1287a32412c426f4a6df
TonyHuynh <huumyit@gmail.com>
2017-12-11 15:03:43 +0700

    Created s/reactjs01/reactjs-router/reactjs-redux-boiler-template (reactjs-router)

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
+import { Prompt } from 'react-router-dom';
 
 class Contact extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isChecked: false
+    }
+  }
+
+  onClick = (isChecked) => {
+    this.setState({
+      isChecked: isChecked
+    });
+  }
+  
   render() {
+    var { isChecked } = this.state;
     return (
       <div className="alert alert-success">
         <strong>Contact page</strong>
+        <br />
+        <button type="button" className="btn btn-success" onClick={() => this.onClick(false)} >Allow</button>
+        <button type="button" className="btn btn-danger" onClick={() => this.onClick(true)} >Do not allow</button>
+
+        <Prompt  
+          when={isChecked}
+          message={ location => (`Are you sure you want to go to ${location.pathname}`) }
+        />
       </div>
-      
     );
   }
 }
